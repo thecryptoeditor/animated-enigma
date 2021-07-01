dbb3f56a1f03d1d782357057778d6c06d6fa0b6c
Admin <jatin.aggarwal.15csc@bml.edu.in>
2021-07-01 22:29:15 +0530

    New header added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,25 @@ import React from 'react'
 
 export const Header = (props) =>{
 	return (
+		<nav class="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+		  	<div class="container-fluid">
+		    	<a class="navbar-brand" href="/">TodoItem</a>
+		    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+		      		<span class="navbar-toggler-icon"></span>
+		    	</button>
 
-		<div className="">
-			<h5 style={rendomText}>{props.Heading}</h5>
-			<h6 style={thanksText}>{props.subHeading}</h6>
-		</div>
-
+		    	<div class="collapse navbar-collapse" id="navbarScroll">
+		      		<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+		        		<li class="nav-item">
+		          			<a class="nav-link active" aria-current="page" href="/home">Home</a>
+		        		</li>
+		        		<li class="nav-item">
+		          			<a class="nav-link active" aria-current="page" href="/about">About</a>
+		        		</li>
+		      		</ul>
+				</div>
+			</div>
+		</nav>
 	)
 }
 
